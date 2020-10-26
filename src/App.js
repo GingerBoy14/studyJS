c432e49a59835736ece287a946d7eb24e0a59885
Rostyslav <rostyslavmarchuk440@gmail.com>
2020-10-26 13:44:37 +0200

    Edited validator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import CONSTANTS from './constants'
-import { Post, Storage } from './classes'
-import PropTypes from './utils/PropTypes'
-import Validator from './utils/validator'
+import React from "react"
+import CONSTANTS from "./constants"
+import { Post, Storage } from "./classes"
+import PropTypes from "./utils/PropTypes"
+import Validator from "./utils/validator"
 
 const post1 = new Post()
 const post2 = new Post()
@@ -10,10 +10,10 @@ Storage.post(post1)
 Storage.post(post2)
 
 const rules = {
-  name: PropTypes.string().required().min(1),
-  age: PropTypes.number()
+  name: PropTypes.string().required().min(1).max(3),
+  age: PropTypes.number(),
 }
-const args = { name: 'sd6', age: 5 }
+const args = { name: "sd6", age: 5 }
 
 Validator.validate({ rules, args })
 const App = () => <h1>asdfasdf</h1>
--- src/utils/validator/Validator.js
+++ src/utils/validator/Validator.js
@@ -3,9 +3,8 @@ class Validator {
     this.validatorFunctions = {
       dataType: this.dataTypeCheck,
       isRequired: this.isRequiredCheck,
-      minValue: this.minCheck,
-      maxValue: this.maxCheck
-      // min: this.checkMinMax('min')
+      minValue: this.checkMinMax,
+      maxValue: this.checkMinMax,
     }
   }
 
@@ -16,61 +15,52 @@ class Validator {
 
   dataTypeCheck(validateParams) {
     const { objProp, ruleValue, objPropKey } = validateParams
-    if (Array.isArray(objProp) && ruleValue === 'array') return true
+    if (Array.isArray(objProp) && ruleValue === "array") return true
     if (typeof objProp === ruleValue) return true
     throw new Error(`${objPropKey} type isn't type that was declared in rules`)
   }
 
-  valueDeterminator = (objProp, dataType, objPropKey) => {
-    const map = {
-      string: objProp && objProp.length,
-      array: objProp && objProp.length,
-      number: objProp
+  checkMinMax = ({
+    objProp,
+    dataType,
+    ruleValue,
+    objPropKey,
+    ruleValueKey,
+  }) => {
+    let valueDeterminator = (objProp, dataType, objPropKey) => {
+      dataType =
+        dataType === "array" || dataType === "string"
+          ? (dataType = "else")
+          : dataType
+      const map = {
+        else: objProp && objProp.length,
+        number: objProp,
+      }
+      if (map[dataType]) return map[dataType]
+      throw new Error(`${objPropKey} cannot have minimum or maximum value`)
     }
-    return map[dataType]
-    throw new Error(
-      `Current ${objPropKey} cannot have minimum or maximum value`
-    )
-  }
-
-  minCheck = (validateParams) => {
-    const { objProp, dataType, ruleValue, objPropKey } = validateParams
-    let tmp = this.valueDeterminator(objProp, dataType)
-    if (tmp >= ruleValue) return true
-    throw new Error(`${objPropKey} is lesser than minimum value`)
-  }
-
-  maxCheck = (validateParams) => {
-    const { objProp, dataType, objPropKey, ruleValue } = validateParams
-    if (this.valueDeterminator(objProp, dataType, objPropKey) <= ruleValue)
-      return true
-    throw new Error(`${objPropKey} is bigger than maximum value`)
-  }
-
-  checkMinMax = (val) => {
-    return () => {
-      // const {objProp, dataType, ruleValue, objPropKey} = validateParams;
-      // let tmp = this.valueDeterminator(objProp, dataType)
-      // if(tmp >= ruleValue) return true;
-      // throw new Error(`${objPropKey} is lesser than minimum value`);
-      // Value determinator
-    }
+    let op =
+      ruleValueKey === "minValue"
+        ? valueDeterminator(objProp, dataType) >= ruleValue
+        : valueDeterminator(objProp, dataType) <= ruleValue
+    if (op) return true
+    else
+      throw new Error(
+        `${objPropKey} is smaller/bigger than minimum/maximum value`
+      )
   }
 
   validate(obj) {
     const { rules, args } = obj
     for (let objPropKey in rules) {
       for (let ruleValueKey of Object.keys(rules[objPropKey])) {
-        if (
-          rules[objPropKey][ruleValueKey] === false ||
-          rules[objPropKey][ruleValueKey] === true
-        )
-          continue
+        if (rules[objPropKey][ruleValueKey] === false) continue
         let validateParams = {
           objProp: args[objPropKey],
           objPropKey,
           ruleValue: rules[objPropKey][ruleValueKey],
-          dataType: rules[objPropKey]['dataType']
+          dataType: rules[objPropKey]["dataType"],
+          ruleValueKey,
         }
         this.validatorFunctions[ruleValueKey](validateParams)
       }
