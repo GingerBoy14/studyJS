42cc4c26d4883fbd827d66678454459f11d7d60a
5P4C3GR33D <cyber.cuttlefish@gmail.com>
2020-10-26 18:07:21 +0200

    small improvements to .eslintrc; added file-system package and app.test.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react"
-import CONSTANTS from "./constants"
-import { Post, Storage } from "./classes"
-import PropTypes from "./utils/PropTypes"
-import Validator from "./utils/validator"
+import React from 'react'
+import CONSTANTS from './constants'
+import { Post, Storage } from './classes'
+import PropTypes from './utils/PropTypes'
+import Validator from './utils/validator'
 
 const post1 = new Post()
 const post2 = new Post()
@@ -14,9 +14,9 @@ Storage.post(post2)
  */
 const rules = {
   name: PropTypes.string().required().min(1).max(3),
-  age: PropTypes.number(),
+  age: PropTypes.number()
 }
-const args = { name: "sd6", age: 5 }
+const args = { name: 'sd6', age: 5 }
 
 Validator.validate({ rules, args })
 const App = () => <h1>asdfasdf</h1>
