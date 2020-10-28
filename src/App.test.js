a08f49ffdf655039a8ce822bd377fb1a46f25a72
GingerBoy14 <maks.27.04.2002@gmail.com>
2020-10-29 00:41:54 +0200

    write tests for test lib

---
+++ src/App.test.js
@@ -0,0 +1,26 @@
+describe('first test', () => {
+  it('should do smth', function () {
+    expect(5).toBe(5)
+    expect(4).toBe(4)
+  })
+})
+
+describe('Math', () => {
+  it('sum return 10', () => {
+    expect(() => 5 + 5).toBe(10)
+    expect(() => 5 + 5).toBe(25)
+  })
+  it('sum return 11', () => {
+    const sum = () => 5 + 2
+    expect(sum()).toBe(11)
+  })
+})
+describe('Text', () => {
+  it('add Hello + World', () => {
+    expect(() => 'Hello World').toBe(10)
+  })
+  it('sum return hello', () => {
+    const sum = () => 'hello'
+    expect(sum()).toBe('hello')
+  })
+})
---
+++ src/dd.test.js
@@ -0,0 +1,6 @@
+// describe('first testsdf', () => {
+//   it('should dfsd smth', function () {
+//     expect(5).toBe(5)
+//     expect(4).toBe(4)
+//   })
+// })
