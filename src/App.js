a340a4f4da2dde064b294439244ea702caf5d6bf
okrdima <okrdima@gmail.com>
2020-11-05 17:07:24 +0200

    add compareFucntions refactoring and create modules fix appReact

--- src/App.js
+++ src/App.js
@@ -10,33 +10,33 @@ const App = () => {
 const Test = () => (
   <div>
     <h1>Result tests</h1>
-    {log &&
-      log.results.map(({ it, nameDescribe }) => (
-        <div>
-          <h2 style={{ color: 'blue' }}> Name Describe = {nameDescribe}</h2>
-          {it.map(({ nameIt, expects }) => (
-            <div>
-              <h2>Name It = {nameIt}</h2>
-              {expects.map(({ expect, status }) => {
-                const pass = () => (
-                  <span style={{ fontSize: 17, fontWeight: '600' }}>✔️</span>
-                )
-                const fail = () => (
-                  <span style={{ fontSize: 17, fontWeight: '600' }}>✖️</span>
-                )
-                return (
-                  <div>
-                    <h3 style={{ color: 'gray', display: 'inline' }}>
-                      {expect}
-                    </h3>
-                    {status ? pass() : fail()}
-                  </div>
-                )
-              })}
-            </div>
-          ))}
-        </div>
-      ))}
+    <h2 style={{ color: 'orange' }}>Total test = {log.totalTests}</h2>
+    <h2 style={{ color: 'red' }}>Failed test = {log.failedTests}</h2>
+    <h2 style={{ color: 'green' }}>Passed test = {log.passedTests}</h2>
+    {log.results.map(({ it, nameDescribe }) => (
+      <div>
+        <h2 style={{ color: 'blue' }}> Name Describe = {nameDescribe}</h2>
+        {it.map(({ nameIt, expects }) => (
+          <div>
+            <h2>Name It = {nameIt}</h2>
+            {expects.map(({ expect, status }) => {
+              const pass = () => (
+                <span style={{ fontSize: 17, fontWeight: '600' }}>✅</span>
+              )
+              const fail = () => (
+                <span style={{ fontSize: 17, fontWeight: '600' }}>❌</span>
+              )
+              return (
+                <div>
+                  <h3 style={{ color: 'gray', display: 'inline' }}>{expect}</h3>
+                  {status ? pass() : fail()}
+                </div>
+              )
+            })}
+          </div>
+        ))}
+      </div>
+    ))}
   </div>
 )
 
