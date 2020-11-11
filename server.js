e90b8b18d6d982f429e239b7a44ad8eaf607eba6
Aleksandr <aleksandr.golyk@gmail.com>
2020-11-11 18:24:59 +0200

    fixed PORT error

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require('express')
 var app = express()
 
+var artists = [
+  {
+    id: 1,
+    name: 'Metallica'
+  },
+  {
+    id: 2,
+    name: 'Iron Maiden'
+  },
+  {
+    id: 3,
+    name: 'Deep Purple'
+  }
+]
+
 app.get('/', function (req, res) {
   res.send('Hello API')
 })
-app.listen(3012, function () {})
-console.log('API app started')
+
+app.get('/artists', function (req, res) {
+  res.send(artists)
+})
+
+app.get('/artists/:id', function (req, res) {
+  console.log(req.params)
+  var artist = artists.find(function (artist) {
+    return artist.id === Number(req.params.id)
+  })
+  res.send(artist)
+})
+app.listen(process.env.PORT)
